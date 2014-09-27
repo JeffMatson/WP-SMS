43f6133a58bb064fb7fa6e5b8fd2841da0ba835f
NikV <techvoltz@gmail.com>
2014-09-26 22:07:06 -0500

    Enqueue styles

--- wp-sms.php
+++ wp-sms.php
@@ -13,3 +13,10 @@ require_once $directory . '/functions.php';
 require_once $directory . '/wp-sms-options.php';
 require_once $directory . '/carrier-list.php';
 require_once $directory . '/alerts/alerts.php';
+
+
+function load_wp_sms_css() {
+	wp_register_style( 'wp-sms-css', get_template_directory_uri() . 'css/wp-sms.css');
+	wp_enqueue_style( 'wp-sms-css' );
+}
+add_action( 'admin_enqueue_scripts', 'load_wp_sms_css' );
